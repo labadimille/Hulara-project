1953f947cb9bcad6dedd443cc6b8ad8d164745f8
labadimille <labadimille@gmail.com>
2026-07-10 21:40:45 +0200

    Create main.py

---
+++ codes/moto-e13-bootloader-unlock-1-try/src/main.py
@@ -0,0 +1,7 @@
+# src/main.py
+def avvia_comando(nome_comando):
+    # Qui integrerai i tuoi script .sh
+    return f"Esecuzione di: {nome_comando} in corso..."
+
+if __name__ == "__main__":
+    print("Hulara Engine Online.")
